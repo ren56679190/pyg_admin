bccb53761a9e302762b9fcb0114ada7560998cb5
ren56679190 <56679190@qq.com>
2019-04-20 14:05:12 +0800

    登录页搭建完成

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,34 +1,56 @@
 <template>
   <div class="login_container">
     <div class="box">
-      <img src="../assets/images/logo.png" alt="">
+      <img src="../assets/images/logo.png" alt>
+      <el-form :model="form">
+        <el-form-item>
+          <el-input placeholder="请输入用户名" v-model="form.username"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input type="password" placeholder="请输入密码" v-model="form.password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button>重置</el-button>
+          <el-button type="primary">登录</el-button>
+        </el-form-item>
+      </el-form>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'login'
-}
+  name: "login",
+  data() {
+    return {
+      form: {
+        username: "",
+        password: ""
+      }
+    };
+  }
+};
 </script>
 
 <style scoped>
-.login_container{
+.login_container {
   width: 100%;
   height: 100%;
-  background: linear-gradient(30deg,#EBF4FB,#115763)
+  background: linear-gradient(30deg, #ebf4fb, #115763);
 }
 .login_container .box {
   width: 400px;
   height: 250px;
-  background:linear-gradient(45deg,#D2ECF1,#efefef);
+  background: linear-gradient(45deg, #d2ecf1, #efefef);
   position: absolute;
   top: 50%;
   right: 50%;
-  transform: translate(50%,-60%);
+  transform: translate(50%, -60%);
   box-shadow: 0 0 10px #eee;
+  padding: 15px;
+  box-sizing: border-box;
 }
-.login_container .box img{
-  margin:15px 115px 0 115px;
+.login_container .box img {
+  margin: 0px 100px 15px 100px;
 }
 </style>
